e5e0e80cc1a6cbd0ec3da1fbd77ea4d32a05f4f8
engti <rbrowne@gmail.com>
2016-11-03 00:14:41 +0530

    Updated viewID logic - find common elements with curated list for definite org accounts

--- 01_data_fetch_ga.r
+++ 01_data_fetch_ga.r
@@ -7,15 +7,25 @@
     
 ## get account info
   account_list <- google_analytics_account_list()
-    account_main <- account_list %>% filter(viewName == "All Web Site Data")  
-
+    # account_main <- account_list %>% filter(viewName == "All Web Site Data") 
+    
+## get another set of accounts    
+    account_curated <- read.csv("account_list_verified.csv",stringsAsFactors = F) %>%
+                        mutate(
+                          Profile.ID = as.character(Profile.ID)
+                        )
+      
+    
+## find common profile ids in account_curated and account_list    
+    account_common <- account_list %>% semi_join(account_curated,c("viewId"="Profile.ID"))
 ## set up the analytics call 
-      for(i in 1:nrow(account_main))
+    ## it uses tryCatch since sometimes the analytics call fails
+      for(i in 1:nrow(account_common))
         {
         print(i)
         errTrp <- tryCatch({
           flag = TRUE
-        tmp <- google_analytics_4(viewId = account_main$viewId[i],
+        tmp <- google_analytics_4(viewId = account_common$viewId[i],
                                   date_range = c("2016-01-03","2016-10-29"),
                                   metrics = c("users","sessions","bounces"),
                                   dimensions = c("week","country"),
@@ -32,7 +42,7 @@
         }, finally = {
          if(flag)
          {
-           tmp$ProfileName <- paste0(account_main$accountName[i],": ",account_main$webPropertyName[i])
+           tmp$ProfileName <- paste0(account_common$accountName[i],": ",account_common$webPropertyName[i])
            if(i == 1){
              df1 <- tmp
            }else if(i > 1 && length(tmp) > 1){
