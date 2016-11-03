abf336b25c2c2672dd9e1b57dd3b00734c93faf8
engti <rbrowne@gmail.com>
2016-11-03 12:43:47 +0530

    Added in ViewID for each row

--- 01_data_fetch_ga.r
+++ 01_data_fetch_ga.r
@@ -13,19 +13,30 @@
     account_curated <- read.csv("account_list_verified.csv",stringsAsFactors = F) %>%
                         mutate(
                           Profile.ID = as.character(Profile.ID)
-                        )
+                        ) %>%
+                        unique() 
       
     
 ## find common profile ids in account_curated and account_list    
-    account_common <- account_list %>% semi_join(account_curated,c("viewId"="Profile.ID"))
+    account_common <- account_list %>% 
+      semi_join(account_curated,c("viewId"="Profile.ID")) 
+    
+    
+    account_common <- merge(account_curated,account_common,by.x = "Profile.ID",by.y = "viewId") %>%
+                      select(-accountId,-UA.number,-Url,-accountName)
+    
+    account_common_focus <- account_common %>%
+                              select(Profile.ID,webPropertyName,Brand,Super.Category,Sub.Category,Market,Region.Market)
+    write.csv(account_common_focus,"account_common_focus.csv",row.names = F)
+    
 ## set up the analytics call 
     ## it uses tryCatch since sometimes the analytics call fails
-      for(i in 1:nrow(account_common))
+      for(i in 1:nrow(account_common_focus))
         {
         print(i)
         errTrp <- tryCatch({
           flag = TRUE
-        tmp <- google_analytics_4(viewId = account_common$viewId[i],
+        tmp <- google_analytics_4(viewId = account_common_focus$Profile.ID[i],
                                   date_range = c("2016-01-03","2016-10-29"),
                                   metrics = c("users","sessions","bounces"),
                                   dimensions = c("week","country"),
@@ -42,7 +53,8 @@
         }, finally = {
          if(flag)
          {
-           tmp$ProfileName <- paste0(account_common$accountName[i],": ",account_common$webPropertyName[i])
+           # tmp$ProfileName <- paste0(account_common_focus$accountName[i],": ",account_common_focus$webPropertyName[i])
+           tmp$ViewID <- account_common_focus$Profile.ID[i]
            if(i == 1){
              df1 <- tmp
            }else if(i > 1 && length(tmp) > 1){
