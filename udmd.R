dcd53455caa1cb0b390bc9fa5669c6302efd0c28
engti <rbrowne@gmail.com>
2016-11-11 14:51:20 +0530

    updated queries, working dplyr syntax

--- udmd.R
+++ udmd.R
@@ -6,5 +6,10 @@
 ## load the db connect script
   source("redshift-connect.R")
   
-## pull some data out  
-
+## pick some data up  
+  requests <- tbl(udmdConnect, "events")
+  df1 <- requests %>% select(geo_country,mkt_medium,collector_tstamp) %>% filter(collector_tstamp=="2016-11-01") %>% collect() 
+  
+  
+  requests <- tbl(udmdConnect, sql("SELECT * from atomic.events WHERE mkt_medium == cpc"))
+##   
